dac103449a7cf228258c5e1280e6c01b17b56bcf
Dikamahard <ikkyboss@gmail.com>
2022-09-20 21:24:26 +0700

    decrypt function added

--- Shift-Cipher/shiftcipher.cpp
+++ Shift-Cipher/shiftcipher.cpp
@@ -38,6 +38,33 @@ string encrypt(string text, int key) {
     return encrypted;
 }
 
+string decrypt(string text, int key) {
+    string decrypted;
+
+    for(int i=0; i< text.length(); i++) {
+        int indexDecrypted;
+
+        if((int)text[i] == 32) {
+            indexDecrypted = (int)text[i];
+        }else if((int)text[i] >= 97 && (int)text[i] <= 122) {  //(huruf kecil)
+            indexDecrypted = (int)text[i] - key;
+            if(indexDecrypted < 97) {
+                indexDecrypted += 26;
+            }
+        }else if((int)text[i] >= 65 && (int)text[i] <= 90) {  //(huruf kapital)
+            indexDecrypted = (int)text[i] - key;
+            if(indexDecrypted < 65) {
+                indexDecrypted += 26;
+            }
+        }
+
+        
+
+        decrypted += (char)indexDecrypted;
+    }
+
+    return decrypted;
+}
 
 int main() {
     string text;
@@ -52,9 +79,8 @@ int main() {
     cout << "\n>> Encrypted Text : ";
     cout << encrypt(text, key) << endl;
 
-   
+    cout << "\n>> Decrypted Text : ";
+    cout << decrypt(encrypt(text, key), key) << endl;
 
-   
-    
   
 }
